bdbc7b0b6c5aa06650b315f62f1cc2d1c626d0a5
pcallum <callumrp10@gmail.com>
2022-05-03 15:28:36 +0100

    Added input and output framework

--- src/main/java/com/sparta/simulator/start/Main.java
+++ src/main/java/com/sparta/simulator/start/Main.java
@@ -1,7 +1,11 @@
 package com.sparta.simulator.start;
 
+import com.sparta.simulator.start.display.Display;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Display.mainDisplay();
     }
 }
