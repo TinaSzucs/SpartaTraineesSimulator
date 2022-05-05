e77719e10a0ec45d1aa3ceb01d53f8e29031f0be
Alex Costi <alex_costi@icloud.com>
2022-05-05 11:41:35 +0100

    trainee bench state

--- src/main/java/com/sparta/simulator/model/Trainee.java
+++ src/main/java/com/sparta/simulator/model/Trainee.java
@@ -5,14 +5,23 @@ import java.util.Random;
 public class Trainee {
 
     private Course courseType;
+    private int monthsTrained = 0;
 
     public Trainee()   {
 
         Random random = new Random();
         this.courseType = Course.values()[random.nextInt(0, Course.values().length)];
+        this.monthsTrained = monthsTrained;
     }
 
     public Course getCourseType() {
         return courseType;
+    }
+
+    public void addMonthTraining()  {
+        monthsTrained++;
+    }
+    public int getMonthsTrained() {
+        return monthsTrained;
     }
 }
