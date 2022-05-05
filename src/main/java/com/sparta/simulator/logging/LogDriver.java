d68c525186d1b8b8b1e1d93a135d75afdfa1bc2c
pcallum <callumrp10@gmail.com>
2022-05-05 08:54:35 +0100

    Added logging

---
+++ src/main/java/com/sparta/simulator/logging/LogDriver.java
@@ -0,0 +1,8 @@
+package com.sparta.simulator.logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogDriver {
+    public static Logger logger = LogManager.getLogger("Logger");
+}
