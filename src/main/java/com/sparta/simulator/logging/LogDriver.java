dea8691f7c32e3c673924c30fa75e889f2441c6f
pcallum <callumrp10@gmail.com>
2022-05-05 08:54:35 +0100

    Rebase

--- src/main/java/com/sparta/simulator/logging/LogDriver.java
+++ src/main/java/com/sparta/simulator/logging/LogDriver.java
@@ -5,8 +5,11 @@ import org.apache.logging.log4j.Logger;
 
 public class LogDriver {
     public static Logger logger = LogManager.getLogger("Logger");
+<<<<<<< HEAD
 
     public static void main(String[] args) {
         logger.info("Program started");
     }
+=======
+>>>>>>> 25392a7 (Rebase)
 }
