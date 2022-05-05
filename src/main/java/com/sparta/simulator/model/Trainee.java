9e3708f8bdc08a4f5bbf1c8c2c3e978f86c374d0
Alex Costi <alex_costi@icloud.com>
2022-05-05 12:16:54 +0100

    time trained for employees

--- src/main/java/com/sparta/simulator/model/Trainee.java
+++ src/main/java/com/sparta/simulator/model/Trainee.java
@@ -5,13 +5,13 @@ import java.util.Random;
 public class Trainee {
 
     private Course courseType;
-    private int monthsTrained = 0;
+    private int monthsTrained;
 
     public Trainee()   {
 
         Random random = new Random();
         this.courseType = Course.values()[random.nextInt(0, Course.values().length)];
-        this.monthsTrained = monthsTrained;
+        this.monthsTrained = 0;
     }
 
     public Course getCourseType() {
